040c9381244b53762898e2f229aa100b5d90f1da
Da0neDatGotAway <43645756+Da0neDatGotAway@users.noreply.github.com>
2025-12-08 03:23:00 -0800

    actually commiting before I'm ready to push for once. just did some stuff I don't want to retype

--- src/main/java/net/enchantoutline/mixin/ModelPart_CuboidMixin.java
+++ src/main/java/net/enchantoutline/mixin/ModelPart_CuboidMixin.java
@@ -6,13 +6,14 @@ import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.ActionResult;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Mixin(ModelPart.Cuboid.class)
 public class ModelPart_CuboidMixin implements ModelPart_CuboidAccessor {
@@ -20,6 +21,46 @@ public class ModelPart_CuboidMixin implements ModelPart_CuboidAccessor {
     @Final
     @Mutable
     public ModelPart.Quad[] sides;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    void enchantOutline$storeOnInit(int u, int v, float x, float y, float z, float sizeX, float sizeY, float sizeZ, float extraX, float extraY, float extraZ, boolean mirror, float textureWidth, float textureHeight, Set<Direction> sides, CallbackInfo ci){
+        this.u = u;
+        this.v = v;
+        this.extraX = extraX;
+        this.extraY = extraY;
+        this.extraZ = extraZ;
+        this.mirror = mirror;
+        this.textureWidth = textureWidth;
+        this.textureHeight = textureHeight;
+        this.initDirections = new HashSet<>(sides);
+    }
+
+    @Unique
+    private int u;
+
+    @Unique
+    private int v;
+
+    @Unique
+    private float extraX;
+
+    @Unique
+    private float extraY;
+
+    @Unique
+    private float extraZ;
+
+    @Unique
+    private boolean mirror;
+
+    @Unique
+    private float textureWidth;
+
+    @Unique
+    private float textureHeight;
+
+    @Unique
+    private Set<Direction> initDirections;
 
     @Override
     public ModelPart.Quad[] enchantOutline$getSides() {
@@ -29,5 +70,50 @@ public class ModelPart_CuboidMixin implements ModelPart_CuboidAccessor {
     @Override
     public void enchantOutline$SetSides(ModelPart.Quad[] newSides) {
         sides = newSides;
+    }
+
+    @Override
+    public int enchantOutline$getU() {
+        return u;
+    }
+
+    @Override
+    public int enchantOutline$getV() {
+        return v;
+    }
+
+    @Override
+    public float enchantOutline$getExtraX() {
+        return extraX;
+    }
+
+    @Override
+    public float enchantOutline$getExtraY() {
+        return extraY;
+    }
+
+    @Override
+    public float enchantOutline$getExtraZ() {
+        return extraZ;
+    }
+
+    @Override
+    public boolean enchantOutline$getMirror() {
+        return mirror;
+    }
+
+    @Override
+    public float enchantOutline$getTextureWidth() {
+        return textureWidth;
+    }
+
+    @Override
+    public float enchantOutline$getTextureHeight() {
+        return textureHeight;
+    }
+
+    @Override
+    public Set<Direction> enchantOutline$getInitDirections() {
+        return new HashSet<>();
     }
 }
--- src/main/java/net/enchantoutline/mixin_accessors/ModelPart_CuboidAccessor.java
+++ src/main/java/net/enchantoutline/mixin_accessors/ModelPart_CuboidAccessor.java
@@ -1,8 +1,20 @@
 package net.enchantoutline.mixin_accessors;
 
 import net.minecraft.client.model.ModelPart;
+import net.minecraft.util.math.Direction;
+
+import java.util.Set;
 
 public interface ModelPart_CuboidAccessor {
     public ModelPart.Quad[] enchantOutline$getSides();
     public void enchantOutline$SetSides(ModelPart.Quad[] newSides);
+    public int enchantOutline$getU();
+    public int enchantOutline$getV();
+    public float enchantOutline$getExtraX();
+    public float enchantOutline$getExtraY();
+    public float enchantOutline$getExtraZ();
+    public boolean enchantOutline$getMirror();
+    public float enchantOutline$getTextureWidth();
+    public float enchantOutline$getTextureHeight();
+    public Set<Direction> enchantOutline$getInitDirections();
 }
